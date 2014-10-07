c5eea6f60a473cc03d5ca35af205a90e03a48035
Jiar <Jiarshengy@gmail.com>
2014-10-07 13:04:19 +0100

    little bug fixes

--- crawler/spiders/GPU.py
+++ crawler/spiders/GPU.py
@@ -1,17 +1,14 @@
 import scrapy
+from scrapy import Spider
+
 
 class GPU(scrapy.Spider):
-    name = "GPU";
+    name = "GPUtest";
     allowed_domains = ["tweakers.net"]
     start_urls = ["http://tweakers.net/pricewatch/416125/msi-geforce-gtx-970-gaming-4g/specificaties/"]
 
-def parse(self, response):
-    '''
+    def parse(self, response):
+        key = response.xpath("//*[@id='tab:specificaties']/table/tr[13]/td[1]/text()").extract()
+        value= response.xpath("//*[@id='tab:specificaties']/table/tr[13]/td[2]/text()").extract()
 
-    :param response:
-    :return:
-    '''
-    item = GPU()
-    item['key'] = response.xpath("//*[@id='tab:specificaties']/table/tr[13]/td[1]/text()").extract()
-    item['value'] =response.xpath("//*[@id='tab:specificaties']/table/tr[13]/td[2]/text()").extract()
-    yield item
+        print key, value
--- crawler/spiders/__init__.py
+++ crawler/spiders/__init__.py
@@ -2,22 +2,7 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
-import scrapy
 
-class GPUtest(scrapy.Spider):
-    name = "GPUtest";
-    allowed_domains = ["tweakers.net"]
-    start_urls = ["http://tweakers.net/pricewatch/416125/msi-geforce-gtx-970-gaming-4g/specificaties/"]
-
-def parse(self, response):
-    '''
-    :param response:
-    :return:
-    '''
-    item = GPU()
-    item['key'] = response.xpath("//*[@id='tab:specificaties']/table/tr[13]/td[1]/text()").extract()
-    item['value'] = response.xpath("//*[@id='tab:specificaties']/table/tr[13]/td[2]/text()").extract()
-    yield item
 
  
  
