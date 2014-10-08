29f6f5fb32db417e8979a1dcddcb226eca573278
Jiar <Jiarshengy@gmail.com>
2014-10-08 14:36:43 +0100

    added some methods and all that good stuff

--- crawler/ParseConfig.py
+++ crawler/ParseConfig.py
@@ -1,21 +1,51 @@
 import ConfigParser
+
 __author__ = 'j'
 class ParseConfig:
     xpathkey = None
     xpathvalue = None
     crawl = None
+    crawlList = 0
 
-
-    config = ConfigParser.ConfigParser()
     try:
+        config = ConfigParser.ConfigParser()
         file = "crawl-conf/GPU.conf" #TODO fix exception
         config.read(file)
+
+        xpathkey = config.get('DEFAULT', 'xpathkey')
+        xpathvalue = config.get('DEFAULT', 'xpathvalue')
+        crawl = config.get('DEFAULT', 'crawl')
     except:
         print "Something went wrong while reading the file: " + file
 
-    xpathkey = config.get('DEFAULT', 'xpathkey')
-    xpathvalue = config.get('DEFAULT', 'xpathvalue')
-    crawl = config.get('DEFAULT', 'crawl')
+    def getConfig(self):
+        config = ConfigParser.ConfigParser()
+        try:
+            file = "crawl-conf/GPU.conf" #TODO fix exception
+            config.read(file)
+        except:
+            print "Something went wrong while reading the file: " + file
 
-    for x in range(0, 33):
-        print xpathkey % x
+        return config
+
+    def __init__(self):
+        pass
+
+    def sumSection(self):
+        conf = ConfigParser.ConfigParser()
+        file = "crawl-conf/GPU.conf"
+        conf.read(file)
+
+        sumCrawl = 0
+        for x in range(1,100):
+            a = "ROW"+str(x)
+            if conf.has_section(a):
+                crawl = conf.get(a, 'crawl')
+                if crawl == "1":
+                    sumCrawl += 1
+        return sumCrawl
+
+
+p = ParseConfig()
+
+print p.sumSection()
