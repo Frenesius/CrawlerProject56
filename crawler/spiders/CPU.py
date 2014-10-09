a7cb1398582197b433504fb03ea2c92a3c572b62
Jiar <Jiarshengy@gmail.com>
2014-10-09 13:51:26 +0100

    Added CPU crawler

--- crawler/spiders/CPU.py
+++ crawler/spiders/CPU.py
@@ -1,19 +1,19 @@
-__author__ = 'j'
 import scrapy
 from scrapy import Spider
 from crawler import ParseConfig
 
 
-class CPU(scrapy.Spider):
+class GPU(scrapy.Spider):
     name = "CPUcrawl";
     allowed_domains = ["tweakers.net"]
-    start_urls = [""]
+    start_urls = ["http://tweakers.net/pricewatch/394885/intel-core-i7-4790k-boxed/specificaties/"]
 
     def parse(self, response):
         p = ParseConfig.ParseConfig()
-        listCrawl = p.getCrawlList()
-
+        listCrawl = p.getCrawlList("crawler/crawl-conf/CPU.conf")
+        print "SECTIONS FOUND: " + str(p.sumSection())
         for x in listCrawl:
-            key = response.xpath("//*[@id='tab:specificaties']/table/tr["+str(x)+"]/td[1]/text()").extract() #
-            value= response.xpath("//*[@id='tab:specificaties']/table/tr["+str(x)+"]/td[2]/text()").extract()   #
-            print key, value
+
+            key = response.xpath(p.getKeyxPath(x, "crawler/crawl-conf/CPU.conf") % x).extract()
+            value= response.xpath(p.getValuexPath(x, "crawler/crawl-conf/CPU.conf") % x ).extract()
+            print "ROW",x, key, value
