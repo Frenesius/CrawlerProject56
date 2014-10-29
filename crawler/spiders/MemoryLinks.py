b4057d7161418336a2cea70d5cfdd1a0feff72e4
Resul42 <resul-g_93@hotmail.com>
2014-10-29 17:56:42 +0100

    link deleted

--- crawler/spiders/MemoryLinks.py
+++ crawler/spiders/MemoryLinks.py
@@ -50,8 +50,7 @@ class MemoryLinks(scrapy.Spider):
     'http://tweakers.net/categorie/545/geheugen-intern/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page=44',
     'http://tweakers.net/categorie/545/geheugen-intern/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page=45',
     'http://tweakers.net/categorie/545/geheugen-intern/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page=46',
-    'http://tweakers.net/categorie/545/geheugen-intern/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page=47',
-    'http://tweakers.net/categorie/545/geheugen-intern/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page=48'
+    'http://tweakers.net/categorie/545/geheugen-intern/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page=47'
     ]
 
     allowed_domains = ["tweakers.net"]
