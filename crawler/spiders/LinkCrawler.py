9d3ffdaeaaddb3b32a9d59c0a76b644537859f10
Jiar <Jiarshengy@gmail.com>
2014-11-14 12:35:35 +0000

    writes crawled links to file

--- crawler/spiders/LinkCrawler.py
+++ crawler/spiders/LinkCrawler.py
@@ -2,55 +2,61 @@ import LinkSpider
 from crawler.filter.LinkManager import ParseLinks
 class CaseLinks(LinkSpider.LinkSpider):
     name = "CASElinks"
+    linksConf = "CASELink"
     linkString = 'http://tweakers.net/categorie/61/behuizingen/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page='
     parseLinks = ParseLinks()
     start_urls = parseLinks.parseLinks(linkString, 16)
     allowed_domains = ["tweakers.net"]
     def parse(self, response,):
-        LinkSpider.LinkSpider().altParse(response)
+        LinkSpider.LinkSpider().altParse(response, self.linksConf)
 
 class MotherboardLinks(LinkSpider.LinkSpider):
     name = "MBlinks"
+    linksConf = "MOTHERBOARDLink"
     linkString = 'http://tweakers.net/categorie/47/moederborden/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page='
     parseLinks = ParseLinks()
     start_urls = parseLinks.parseLinks(linkString, 12)
     allowed_domains = ["tweakers.net"]
     def parse(self, response,):
-        LinkSpider.LinkSpider().altParse(response)
+        LinkSpider.LinkSpider().altParse(response, self.linksConf)
 
 class MemoryLinks(LinkSpider.LinkSpider):
     name = "RAMlinks"
+    linksConf = "MEMORYLink"
     linkString ='http://tweakers.net/categorie/545/geheugen-intern/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page='
     parseLinks = ParseLinks()
     start_urls = parseLinks.parseLinks(linkString, 48)
     allowed_domains = ["tweakers.net"]
     def parse(self, response,):
-        LinkSpider.LinkSpider().altParse(response)
+        LinkSpider.LinkSpider().altParse(response, self.linksConf)
 
 class GrapihcsCardLinks(LinkSpider.LinkSpider):
     name = "GPUlinks"
+    linksConf = "GPULink"
     linkString ='http://tweakers.net/categorie/49/videokaarten/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page='
     parseLinks = ParseLinks()
     start_urls = parseLinks.parseLinks(linkString, 12)
     allowed_domains = ["tweakers.net"]
     def parse(self, response,):
-        LinkSpider.LinkSpider().altParse(response)
+        LinkSpider.LinkSpider().altParse(response, self.linksConf)
 
 class ProcessorLinks(LinkSpider.LinkSpider):
     name = "CPUlinks"
+    linksConf = "CPULink"
     linkString =  'http://tweakers.net/categorie/46/processors/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page='
     parseLinks = ParseLinks()
     start_urls = parseLinks.parseLinks(linkString, 27)
     allowed_domains = ["tweakers.net"]
     def parse(self, response,):
-        LinkSpider.LinkSpider().altParse(response)
+        LinkSpider.LinkSpider().altParse(response, self.linksConf)
 
 class PowerSupplyLinks(LinkSpider.LinkSpider):
     name = "PSUlinks"
+    linksConf = "PSULink"
     linkString = 'http://tweakers.net/categorie/664/voedingen/producten/?currFilters=q1YqSExPDc6sSlWyMjQwqAUA&pageSize=100&page='
     parseLinks = ParseLinks()
     start_urls = parseLinks.parseLinks(linkString, 16)
     allowed_domains = ["tweakers.net"]
     def parse(self, response,):
-        LinkSpider.LinkSpider().altParse(response)
+        LinkSpider.LinkSpider().altParse(response, self.linksConf)
 
--- crawler/spiders/LinkSpider.py
+++ crawler/spiders/LinkSpider.py
@@ -1,19 +1,19 @@
 import re
 import scrapy
-from crawler.filter.LinkManager import ParseLinks
 
 
 class LinkSpider(scrapy.Spider):
     name = " "
-    linkString = ' '
+    linkString = " "
+    linksConf1 = " "
     linksArr = []
     start_urls = None
     allowed_domains = ["tweakers.net"]
 
     def parse(self, response,):
-        self.altParse(response)
+        self.altParse(response, None)
         
-    def altParse(self, response):
+    def altParse(self, response, linksConf1):
         for x in range(1,101):
             url = response.xpath('//*[@id="compareProductListing"]/table/tr[%s]/td[3]/p[2]/a/@href' % x).extract()
             pattern = r"(\[\])"
@@ -21,4 +21,9 @@ class LinkSpider(scrapy.Spider):
                 continue
             else:
                 self.linksArr.append(url)
+        f = open('crawler/link-config/' + linksConf1, 'w')
+        for x in range(len(self.linksArr)):
+            f.write(str(self.linksArr[x]) + "\n")
+        f.close()
     print len(linksArr)
+
