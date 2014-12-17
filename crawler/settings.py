7af67888da6c4265b6fbdee4b60c4d57b307d37a
Caner Y <Jiar@users.noreply.github.com>
2014-12-17 22:11:01 +0100

    Removed the retry on 404 http

--- crawler/settings.py
+++ crawler/settings.py
@@ -18,7 +18,7 @@ COOKIES_ENABLED = False
 # Retry many times since proxies often fail
 RETRY_TIMES = 10000000
 # Retry on most error codes since proxies fail for different reasons
-RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 408]
 
 #LOG_LEVEL = "INFO"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
