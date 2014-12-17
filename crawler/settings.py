58657d37ba5afe64adacf3fcbc029903ab188f93
Jiar <Jiarshengy@gmail.com>
2014-12-17 16:54:04 +0000

    Changed crawl settings

--- crawler/settings.py
+++ crawler/settings.py
@@ -16,10 +16,9 @@ DOWNLOAD_DELAY = 3
 COOKIES_ENABLED = False
 
 # Retry many times since proxies often fail
-RETRY_TIMES = 10
+RETRY_TIMES = 10000000
 # Retry on most error codes since proxies fail for different reasons
 RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
-PROXY_LIST = 'crawler/configs/proxylist.txt'
 
 #LOG_LEVEL = "INFO"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
